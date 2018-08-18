aca2d08c25a1589d7157e1fbe6c74629d13a7098
Nayan <nayan.vekariya014@gmail.com>
2018-08-18 10:40:37 +0530

    basic commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,49 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-home',
+  templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  myForm: FormGroup;
 
-  constructor(public navCtrl: NavController) {
+  isName: boolean = false;
+  isEmail: boolean = false;
+  isPhone: boolean = false;
+  isPassword: boolean = false;
 
-  }
+  constructor(
+    public navCtrl: NavController,
+    public fb: FormBuilder,) {
+    this.myForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['',[Validators.required, Validators.maxLength(10), Validators.minLength(10)]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  validate(data){
+    if(data == 'name'){
+      this.isName = true;
+    }
+    else if(data == 'email'){
+      this.isEmail = true;
+    }
+    else if(data == 'phone'){
+      this.isPhone = true;
+    }
+    else if(data == 'password'){
+      this.isPassword = true;
+    }
+  }
+
+  submit(){
+
+  }
 
 }
